71b8908f93c99194bc3ce69aac4feca8663c619a
David Caballero <d-knight@Davids-Mac-mini.fritz.box>
2021-12-24 08:22:22 +0100

    Version 2.0-Total refactor

--- src/script.js
+++ src/script.js
@@ -1,21 +1,34 @@
 import './style.css'
-import * as THREE from 'three'
-import * as dat from 'lil-gui'
-import gsap from 'gsap'
 
 /**
- * Debug
- */
-
-/**
+ * 
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+//const canvas = document.querySelector('canvas.webgl')
 
-// Scene
-const scene = new THREE.Scene()
+//Scene
+function init() {
+        scene = new THREE.Scene();
+        renderer = new THREE.WebGLRenderer({alpha:true,antialias:true});
+        renderer.setSize(window.innerWidth,window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+        camera = new THREE.PerspectiveCamera(50,window.innerWidth/window.innerHeight,1,1000);
+        camera.position.set(0,0,500);
+        controls = new OrbitControls(camera, renderer.domElement);
+        
+        pointlight = new THREE.PointLight(0xffffff,1);
+        pointlight.position.set(200,200,200);
+        scene.add(pointlight);
+        let ballGeo = new THREE.SphereGeometry(100,64,64);
+        let ballMat = new THREE.MeshPhysicalMaterial(ballMaterial);
+        let ballMesh = new THREE.Mesh(ballGeo,ballMat);
+        scene.add(ballMesh);
+        animate(); // rendering loop
+}
 
+
+// Text
 const fontLoader = new FontLoader()
 
 fontLoader.load(
